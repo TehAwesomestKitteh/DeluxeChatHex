44a1124200514f4945832e288f269224af181c58
yakovliam <jacob@yakovliam.com>
2021-06-28 19:16:31 -0400

    :bug: Fix build.gradle with new custom deluxechat api

--- src/main/java/com/yakovliam/deluxechathex/DeluxeChatHex.java
+++ src/main/java/com/yakovliam/deluxechathex/DeluxeChatHex.java
@@ -4,7 +4,6 @@ import com.yakovliam.deluxechathex.builder.live.NormalLiveChatFormatBuilder;
 import com.yakovliam.deluxechathex.converter.deluxeformat.DeluxeFormatConverter;
 import com.yakovliam.deluxechathex.model.formatting.ChatFormat;
 import com.yakovliam.deluxechathex.util.Triple;
-import io.papermc.paper.event.player.AsyncChatEvent;
 import me.clip.deluxechat.events.DeluxeChatEvent;
 import me.clip.deluxechat.objects.DeluxeFormat;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
@@ -16,8 +15,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Collections;
 
 public final class DeluxeChatHex extends JavaPlugin implements Listener {
 
